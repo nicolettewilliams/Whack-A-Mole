4c9ed5431006b4faa1732ffd23d56258a435558d
nicolette <nicolette.williams8@icloud.com>
2016-04-13 09:54:34 -0500

    updated whack a mole stuff

--- public/js/whack_a_mole.js
+++ public/js/whack_a_mole.js
@@ -1,35 +1,44 @@
 $(document).ready(function() {
 
     var nuts = 0;
-    var gameOverTimer = 30;
+    var gameOverTimer = 31;
     var outdoor = new Audio("/audio/outdoor.mp3");
+    var missedAudio = null;
 
     outdoor.play();
 
+    // starting the game
     $('.start').click(function() {
-        $('.molehole').show();
+        $('.middleDiv').show();
         $('.start').hide();
         play();
     });
 
+    // function to make the game work
     function play(){
-        setInterval(function() {
+        var endGame = setInterval(function() {
             if (gameOverTimer == 0) {
                 clearInterval(game);
+                (gameOverTimer)
+                clearInterval(endGame);
                 outdoor.pause();
+                // modal functionality
                 $('#dialog').modal('show');
-                $('.modal-body').html('You grabbed ' + nuts + ' nuts. Do you want to play again?')
+                $('.modal-body').html('You grabbed ' + nuts + ' squirrel nuts. Do you want to play again?')
                 $('#yes').click(function(){
                     window.location.reload();
                 });
             } 
+            // countdown for game
+            $('#countDown').html(' ' + gameOverTimer + ' seconds...');
             gameOverTimer--;
         }, 1000);
-        $('#timer').html(gameOverTimer);
 
         var game = setInterval(function() {
 
+            // gets random squirrel to pop up
             randomHole = Math.floor((Math.random() * 8) + 1);
+            // random audio when squirrel is clicked
             randomClicked = Math.floor((Math.random() * 4) + 1);
             randomSelected = $('.molehole' + randomHole);
 
@@ -39,34 +48,43 @@ $(document).ready(function() {
 
             animateSquirrel(id, randomSelected);
 
-            // Adds a listener to the current molehole
-            randomSelected.on('click', function(){
-
-                $(this).removeClass('active' + randomHole);
-                $('#gathered').html('Nuts Grabbed: ' + nuts); 
-                new Audio("/audio/clicked" + randomClicked + ".wav").play();
-
-            });
-
-            setTimeout(function() {
-                randomMissed = Math.floor((Math.random() * 3) + 1);
+            // function for missed squirrels
+            missed = setTimeout(function() {
+                
                 if(randomSelected.hasClass('active' + randomHole)){
-                    new Audio("/audio/missed" + randomMissed + ".wav").play();
+                    missedAudio = new Audio("/audio/missed" + randomMissed + ".wav").play();
                 }
 
                 randomSelected.removeClass('active' + randomHole);
             }, 1990);
 
+            // changes nut image depending on how many squirrels you have clicked
+            randomSelected.on('click', function(){
+        
+                $(this).removeClass('active' + randomHole);
+                if (nuts >= 8){
+                    $('#gathered').html('Nuts Grabbed: ' + nuts + '<br><br>' + '<img src="/img/bignuts.png">');
+                } else if (nuts >= 4) {
+                    $('#gathered').html('Nuts Grabbed: ' + nuts + '<br><br>' + '<img src="/img/smallnuts.png">');
+                } else {
+                    $('#gathered').html('Nuts Grabbed: ' + nuts + '<br><br>' + '<img src="/img/nut.png">');
+                }
+                new Audio("/audio/clicked" + randomClicked + ".wav").play();
+                missedAudio.pause();
+
+            });
+
         }, 2000);
     }  
 
+    // increments the score
     $('.img').on('click', function() {
 
         nuts++
+
     }) ;
 
-    $('#timer').html(gameOverTimer)
-
+    // animation for all squirrel images
     function animateSquirrel(id, squirrel) {
         
         switch (id) {
@@ -138,5 +156,3 @@ $(document).ready(function() {
     };
 
 });
-
-
