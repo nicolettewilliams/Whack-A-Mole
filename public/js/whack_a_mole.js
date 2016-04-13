2791ec84273e6614f681404eba87a7a036d0caf0
nicolette <nicolette.williams8@icloud.com>
2016-04-13 13:11:10 -0500

    completed mole game. updated picture names.

--- public/js/whack_a_mole.js
+++ public/js/whack_a_mole.js
@@ -3,18 +3,17 @@ $(document).ready(function() {
     var nuts = 0;
     var gameOverTimer = 31;
     var outdoor = new Audio("/audio/outdoor.mp3");
-    var missedAudio = null;
 
     outdoor.play();
 
-    // starting the game
+    // starts the game
     $('.start').click(function() {
         $('.middleDiv').show();
         $('.start').hide();
         play();
     });
 
-    // function to make the game work
+    // makes the game work
     function play(){
         var endGame = setInterval(function() {
             if (gameOverTimer == 0) {
@@ -22,45 +21,54 @@ $(document).ready(function() {
                 (gameOverTimer)
                 clearInterval(endGame);
                 outdoor.pause();
-                // modal functionality
                 $('#dialog').modal('show');
-                $('.modal-body').html('You grabbed ' + nuts + ' squirrel nuts. Do you want to play again?')
+
+                // if nuts equals 1 then it'll say nut instead of nuts
+                if (nuts == 1) {
+                    $('.modal-body').html('You grabbed ' + nuts + ' squirrel nut. <br> Do you want to play again?');
+                } else {
+                    $('.modal-body').html('You grabbed ' + nuts + ' squirrel nuts. <br> Do you want to play again?');
+                };
+
+                // reloads page when yes is clicked
                 $('#yes').click(function(){
                     window.location.reload();
                 });
             } 
-            // countdown for game
+            // puts the counddown in the HTML
             $('#countDown').html(' ' + gameOverTimer + ' seconds...');
             gameOverTimer--;
         }, 1000);
 
         var game = setInterval(function() {
 
-            // gets random squirrel to pop up
+            // picks a random squirrel number
             randomHole = Math.floor((Math.random() * 8) + 1);
-            // random audio when squirrel is clicked
+            // random audio number for when squirrel is clicked
             randomClicked = Math.floor((Math.random() * 4) + 1);
+            // random audio number for when squirrel is missed
+            randomMissed = Math.floor((Math.random() * 3) + 1);
             randomSelected = $('.molehole' + randomHole);
 
+            // random selected squirrels data
             var id = randomSelected.data('moleid');
 
             randomSelected.addClass('active' + randomHole);
 
             animateSquirrel(id, randomSelected);
 
-            // function for missed squirrels
             missed = setTimeout(function() {
                 
                 if(randomSelected.hasClass('active' + randomHole)){
-                    missedAudio = new Audio("/audio/missed" + randomMissed + ".wav").play();
+                    new Audio("/audio/missed" + randomMissed + ".wav").play();
                 }
 
                 randomSelected.removeClass('active' + randomHole);
             }, 1990);
 
-            // changes nut image depending on how many squirrels you have clicked
+            // changes nut images depending on how many squirrels clicked
             randomSelected.on('click', function(){
-        
+
                 $(this).removeClass('active' + randomHole);
                 if (nuts >= 8){
                     $('#gathered').html('Nuts Grabbed: ' + nuts + '<br><br>' + '<img src="/img/bignuts.png">');
@@ -70,21 +78,18 @@ $(document).ready(function() {
                     $('#gathered').html('Nuts Grabbed: ' + nuts + '<br><br>' + '<img src="/img/nut.png">');
                 }
                 new Audio("/audio/clicked" + randomClicked + ".wav").play();
-                missedAudio.pause();
-
             });
 
         }, 2000);
     }  
 
-    // increments the score
+    // increments score
     $('.img').on('click', function() {
 
         nuts++
-
     }) ;
 
-    // animation for all squirrel images
+    // all animations for squirrels
     function animateSquirrel(id, squirrel) {
         
         switch (id) {
@@ -99,9 +104,9 @@ $(document).ready(function() {
                 break;
             case 2:
                 squirrel.animate({
-                    top: '+=340px'
+                    top: '+=458px'
                 }, 1000).animate({
-                    top: '-=340px',
+                    top: '-=458px',
                 }, 990);
                 break;
             case 3:
@@ -129,26 +134,26 @@ $(document).ready(function() {
                 break;
             case 6:
                 squirrel.animate({
-                    bottom: '+=458px',
+                    bottom: '+=550px',
                     left: '+=359px'
                 }, 1000).animate({
-                    bottom: '-=458px',
+                    bottom: '-=550px',
                     left: '-=359px'
                 }, 990);
                 break;
             case 7:
                 squirrel.animate({
-                    bottom: '+=340px'
+                    bottom: '+=550px'
                 }, 1000).animate({
-                    bottom: '-=340px',
+                    bottom: '-=550px',
                 }, 990)
                 break;
             case 8:
                 squirrel.animate({
-                    bottom: '+=458px',
+                    bottom: '+=550px',
                     right: '+=329px'
                 }, 1000).animate({
-                    bottom: '-=458px',
+                    bottom: '-=550px',
                     right: '-=359px'
                 }, 990);
                 break;
